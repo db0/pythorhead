97b153bf6da5891bf22d3a2e042e1d2adaa627ad
db0 <mail@dbzer0.com>
2023-06-22 00:51:13 +0200

    feat: nodeinfo

--- pythorhead/requestor.py
+++ pythorhead/requestor.py
@@ -23,9 +23,27 @@ REQUEST_MAP = {
 
 
 class Requestor:
+    nodeinfo: dict = None
+    domain: Optional[str] = None
+
     def __init__(self):
         self._auth = Authentication()
         self.set_api_base_url = self._auth.set_api_base_url
+
+    def set_domain(self, domain: str):
+        self.domain = domain
+        self._auth.set_api_base_url(f"{self.domain}/api/v3")
+        try:
+            self.nodeinfo = requests.get(f"{self.domain}/nodeinfo/2.0.json", timeout=2).json()
+        except Exception as err:
+            logger.error(f"Problem encountered retrieving Lemmy nodeinfo: {err}")
+            return
+        software = self.nodeinfo.get("software",{}).get("name")
+        if software != "lemmy":
+            logger.error(f"Domain name does not appear to contain a lemmy software, but instead '{software}")
+            return
+        logger.info(f"Connected succesfully to Lemmy v{self.nodeinfo['software']['version']} instance {self.domain}")
+
 
     def request(self, method: Request, endpoint: str, **kwargs) -> Optional[dict]:
         logger.info(f"Requesting {method} {endpoint}")
