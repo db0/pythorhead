57147d449500cd037cd1bcba0d1e3454b0e743c0
Divided by Zer0 <mail@dbzer0.com>
2023-06-21 17:32:07 +0200

    feat: user get (#14)
    * feat: user get
    
    * fix: report on fail

---
+++ pythorhead/user.py
@@ -0,0 +1,36 @@
+from typing import Any, List, Literal, Optional
+
+from pythorhead.requestor import Request, Requestor
+from pythorhead.types import PostSortType
+
+
+class User:
+    def __init__(self):
+        self._requestor = Requestor()
+
+    def get(
+        self,
+        person_id: Optional[str] = None,
+        username: Optional[str] = None,
+        sort: Optional[PostSortType] = None,
+        page: Optional[int] = None,
+        limit: Optional[int] = None,
+        community_id: Optional[int] = None,
+        saved_only: Optional[bool] = None,
+    ) -> Optional[dict]:
+        """
+        Get user details with various filters.
+
+        Args:
+            person_id (Optional[str], optional): Defaults to None.
+            username (Optional[str], optional): Defaults to None.
+            sort (Optional[CommentSortType], optional): Defaults to None.
+            page (Optional[int], optional): Defaults to None.
+            limit (Optional[int], optional): Defaults to None.
+            community_id (Optional[int], optional): Defaults to None.
+            saved_only (Optional[bool], optional): Defaults to None.
+        Returns:
+            dict: user view
+        """
+        params: dict[str, Any] = {key: value for key, value in locals().items() if value is not None and key != 'self'}
+        return self._requestor.request(Request.GET, "/user", params=params)
