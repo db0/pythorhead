55b0337ec77f734101dc194bbe45f282a10cfbd1
thekingoflorda <64592718+thekingoflorda@users.noreply.github.com>
2024-02-09 23:44:12 +0100

    feat: added community.ban_user (#89)

--- pythorhead/community.py
+++ pythorhead/community.py
@@ -1,5 +1,6 @@
 from typing import Any, List, Optional, Union
 
+from datetime import datetime
 from pythorhead.requestor import Request, Requestor
 from pythorhead.types import LanguageType, ListingType, SortType
 
@@ -160,3 +161,43 @@ class Community:
             "person_id": person_id
         }
         return self._requestor.api(Request.POST, "/community/mod", json=addmodtocommunity)
+    
+    def ban_user(
+            self, 
+            ban: bool = True, 
+            expires: Optional[Union[datetime, int]] = None, 
+            person_id: int = None,
+            community_id: int = None,
+            reason: Optional[str] = None, 
+            remove_data: Optional[bool] = None
+        ) -> Optional[dict]:
+        """
+
+        Ban user from community
+
+        Args:
+            ban (bool): Defaults to True. False Unbans the user
+            expires (Optional[Union[datetime, int]]): Unix time of ban expiration as an integer or a datetime object. Defaults to None for permanent ban.
+            person_id (int): Defaults to None
+            community_id (int): Defaults to None
+            reason (Optional[str]): Defaults to None
+            remove_data (Optional[bool]): Defaults to None 
+        
+        Returns:
+            Optional[dict]: 
+        """
+    
+        banFromCommunity: dict[str, Any] = {"ban": ban, "person_id": person_id, "community_id": community_id}
+
+        if expires is not None:
+            if isinstance(expires, datetime):
+                # Convert the datetime to Unix time
+                expires = int(expires.timestamp())
+            banFromCommunity["expires"] = expires
+        if reason is not None:
+            banFromCommunity["reason"] = reason
+        if remove_data is not None:
+            banFromCommunity["remove_data"] = remove_data
+
+        return self._requestor.api(Request.POST, "/community/ban_user", json=banFromCommunity)
+
