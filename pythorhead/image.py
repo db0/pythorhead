1ac87a064fd97ba2829e436d5e41681ae6576906
Krafting <36538123+Krafting@users.noreply.github.com>
2024-05-04 16:37:38 +0200

    Undo image.py

--- pythorhead/image.py
+++ pythorhead/image.py
@@ -56,9 +56,6 @@ class Image:
                 del file["delete_token"]
 
             return data["files"]
-
-        else:
-            return data
         
     def delete(self, image_delete_url: str):
         """
